1cd3e349ca73baee2e27bb2c78698d413b398ef2
juanramirezbl <najublanajubla@gmail.com>
2025-12-11 00:26:56 +0100

    Initial Commit

---
+++ optic_detec/optic_detecApp.swift
@@ -0,0 +1,17 @@
+//
+//  optic_detecApp.swift
+//  optic_detec
+//
+//  Created by Juan Ramirez Blanco on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct optic_detecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ optic_detecTests/optic_detecTests.swift
@@ -0,0 +1,17 @@
+//
+//  optic_detecTests.swift
+//  optic_detecTests
+//
+//  Created by Juan Ramirez Blanco on 11/12/25.
+//
+
+import Testing
+@testable import optic_detec
+
+struct optic_detecTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
